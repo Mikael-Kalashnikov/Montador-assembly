1d624f524b18e7aafde01b5e4a691edd33777b98
Mikael-Kalashnikov <mikaelalvez5@gmail.com>
2022-09-23 08:28:13 -0300

    type I and J

--- main.cpp
+++ main.cpp
@@ -7,11 +7,35 @@
 
 using namespace std;
 
+R r[18] = { {"sll", 0}, {"srl", 2}, {"jr", 8}, {"mtfhi", 16}, {"mthflo", 18}, {"mult", 24}, {"multu", 25},{ "div", 26},{ "divu", 27}, {"add", 32}, {"addu", 33}, {"sub", 34}, {"subu", 35}, {"and", 36},{ "or", 37},{ "slt", 43}, {"sltu", 43}, {"mul", 2, 28} };
+I i[11] = { {"beq", 4}, {"bne", 5}, {"addi", 8}, {"addiu", 9}, {"slti", 10}, {"sltiu", 11}, {"andi", 12}, {"ori", 13}, {"lui", 15}, {"lw", 35}, {"sw", 43} };
+J j[2] = { {"j", 2}, {"jal", 3} };
+
+string registradores[32] = { "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$s8","$ra" };
+
 unsigned valueInstructionR(ifstream&, string);
+unsigned valueInstructionI(ifstream&, string);
+unsigned valueInstructionJ(ifstream&, string);
 
 bool isR(string str) {
 	for (int i = 0; i < 16; i++) {
 		if (str == r[i].type)
+			return true;
+	}
+	return false;
+}
+
+bool isI(string str) {
+	for (int k = 0; k < 11; k++) {
+		if (str == i[k].type)
+			return true;
+	}
+	return false;
+}
+
+bool isJ(string str) {
+	for (int i = 0; i < 2; i++) {
+		if (str == j[i].type)
 			return true;
 	}
 	return false;
@@ -65,25 +89,10 @@ int main()
 		regex regInst("[0-9$:]");
 
 		if (!regex_search(aux, regInst))
-			if(isR(aux)){
-				valueInstructionR(fin, aux);
-			}
+			if (isR(aux))
+				cout << valueInstructionR(fin, aux) << endl;
 			else if (isI(aux))
-				for(int k = 0; k < 11; k++){
-					if(aux == i[k].type){
-						value = value | i[k].op;
-
-						for(int j = 0; j < 2; j++){
-							fin >> aux; // $19,
-							value = value << 5;
-							// value = value | std::stoi(aux); 
-						}
-
-						fin >> aux;
-						value = value << 16;
-						// value = value | std::stoi(aux);
-					}
-				}
+				cout << valueInstructionI(fin, aux) << endl;
 			else if(isJ(aux))
 				for(int i = 0; i < 2; i++){
 					if(aux == j[i].type){
@@ -167,4 +176,37 @@ unsigned valueInstructionR(std::ifstream& fin, string aux) {
 			value = value | func;
 		}
 	return value;
+}
+
+unsigned valueInstructionI(ifstream& fin, string aux) {
+	unsigned regs[2];
+	int value = 0;
+	for (int j = 0; j < 11; j++) {
+		if (aux == i[j].type) {
+			value = value | i[j].op;
+			value = value << 6;
+
+			for (int k = 0; k < 2; k++) {
+				fin >> aux;
+				aux.erase(3, 3);
+				int regis;
+				for (regis = 0; regis < 32; regis++)
+					if (aux == registradores[regis])
+						break;
+				regs[k] = regis;
+			}
+
+			value = value | regs[1];
+			value = value << 5;
+
+			value = value | regs[0];
+			value = value << 5;
+
+			fin >> aux;
+			value = value | stoi(aux);
+
+			cout << value << endl;
+		}
+		return value;
+	}
 }
--- matrix.cpp
+++ matrix.cpp
@@ -24,25 +24,9 @@ struct Label {
   short line;
 };
 
-string registradores[32] = { "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$s8","$ra" };
 
-R r[18] = { {"sll", 0}, {"srl", 2}, {"jr", 8}, {"mtfhi", 16}, {"mthflo", 18}, {"mult", 24}, {"multu", 25},{ "div", 26},{ "divu", 27}, {"add", 32}, {"addu", 33}, {"sub", 34}, {"subu", 35}, {"and", 36},{ "or", 37},{ "slt", 43}, {"sltu", 43}, {"mul", 2, 28} };
-I i[11]{};
-J j[2]{};
 
-bool isI(string str) {
-  for(int k = 0; k < 11; k++){
-    if(str == i[k].type)
-      return true;
-  }
-  return false;
-}
 
-bool isJ(string str){
-  for(int i = 0; i < 2; i++){
-    if(str == j[i].type)
-      return true;
-  }
-  return false;
-}
+
+
     
