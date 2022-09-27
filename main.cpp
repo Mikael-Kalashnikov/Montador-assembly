4dafe9bb630f8821f2588b05f3c0e2a58c37da14
ThiagoHenriqueFP <thiagohp070@gmail.com>
2022-09-27 07:39:24 -0300

    Protoype completed

--- main.cpp
+++ main.cpp
@@ -3,43 +3,46 @@
 #include <string>
 #include <regex>
 
-#include "matrix.cpp"
-
 using namespace std;
 
-R r[18] = { {"sll", 0}, {"srl", 2}, {"jr", 8}, {"mtfhi", 16}, {"mthflo", 18}, {"mult", 24}, {"multu", 25},{ "div", 26},{ "divu", 27}, {"add", 32}, {"addu", 33}, {"sub", 34}, {"subu", 35}, {"and", 36},{ "or", 37},{ "slt", 43}, {"sltu", 43}, {"mul", 2, 28} };
-I i[11] = { {"beq", 4}, {"bne", 5}, {"addi", 8}, {"addiu", 9}, {"slti", 10}, {"sltiu", 11}, {"andi", 12}, {"ori", 13}, {"lui", 15}, {"lw", 35}, {"sw", 43} };
-J j[2] = { {"j", 2}, {"jal", 3} };
+struct R {
+	string type;
+	short funct;
+	short op{};
+};
+
+struct I
+{
+	string type;
+	short op;
+};
+
+struct J {
+	string type;
+	short op;
+};
+
+struct Label {
+	string label;
+	int line;
+};
+
+unsigned valueInstructionR(ifstream&, string, int&);
+unsigned valueInstructionI(ifstream&, string, vector<Label>, int&);
+unsigned valueInstructionJ(ifstream&, string, vector<Label>, int&);
+
+bool isR(string);
+bool isI(string);
+bool isJ(string);
+
+R typeR[18] = { {"sll", 0}, {"srl", 2}, {"jr", 8}, {"mtfhi", 16}, {"mthflo", 18}, {"mult", 24}, {"multu", 25},{ "div", 26},{ "divu", 27}, {"add", 32}, {"addu", 33}, {"sub", 34}, {"subu", 35}, {"and", 36},{ "or", 37},{ "slt", 43}, {"sltu", 43}, {"mul", 2, 28} };
+I typeI[11] = { {"beq", 4}, {"bne", 5}, {"addi", 8}, {"addiu", 9}, {"slti", 10}, {"sltiu", 11}, {"andi", 12}, {"ori", 13}, {"lui", 15}, {"lw", 35}, {"sw", 43} };
+J typeJ[2] = { {"j", 2}, {"jal", 3} };
 
 string registradores[32] = { "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$s8","$ra" };
 
-unsigned valueInstructionR(ifstream&, string);
-unsigned valueInstructionI(ifstream&, string);
-unsigned valueInstructionJ(ifstream&, string);
+const int initialAddress = 0x00400000;
 
-bool isR(string str) {
-	for (int i = 0; i < 16; i++) {
-		if (str == r[i].type)
-			return true;
-	}
-	return false;
-}
-
-bool isI(string str) {
-	for (int k = 0; k < 11; k++) {
-		if (str == i[k].type)
-			return true;
-	}
-	return false;
-}
-
-bool isJ(string str) {
-	for (int i = 0; i < 2; i++) {
-		if (str == j[i].type)
-			return true;
-	}
-	return false;
-}
 
 // Colocar na matriz
 // Ler linhas e colocar na matriz
@@ -47,13 +50,11 @@ bool isJ(string str) {
 
 int main()
 {
-	unsigned labelCount = 0;
-	string arquivo = "test.asm";
+	vector<Label> l;
+	cout << "Informe o nome do arquivo ou o caminho ate ele: ";
+	string arquivo;
+	cin >> arquivo;
 	std::ifstream inFile(arquivo);
-	//	std::count(std::istreambuf_iterator<char>(inFile),
-	//           std::istreambuf_iterator<char>(), '\n');
-	// cout << "Informe o nome do arquivo que contem o codigo assembly: ";
-	// cin >> arquivo;
 
 	/*Abre o arquivo com o nome informado*/
 	ifstream fin;
@@ -64,6 +65,7 @@ int main()
 		cout << "A abertura do arquivo falhou!" << endl;
 		exit(EXIT_FAILURE);
 	}
+	int line = 0;
 	while (!fin.eof())
 	{
 		string aux;
@@ -71,52 +73,134 @@ int main()
 
 		regex reg(":");
 		if (regex_search(aux, reg)){
-			//cout << aux;
-			// Contar linha do label
-			// Retirar ultimo caracter ':' e salvar numa matriz 
-			labelCount++;
+			aux.pop_back();
+			l.push_back({ aux, (line * 4) + initialAddress });
 		}
-
-		// Pegar a linha do label
+		char checker;
+		do {
+			 checker = fin.get();
+		}
+		while (checker != '\n' && !fin.eof()); // vai rodar at� pegar a quebra de linha
+		line++;
 	}
+
 	fin.close();
+	
+	ofstream fout;
+	char res;
+	cout << "Deseja salvar o arquivo em qual formato?" << endl;
+	do {
+		cout << "[1] - Binario\n[2] - Hexadecimal  :_\b";
+		cin >> res;
+	} while (res != '1' && res != '2');
+
+	res == '1' ? fout.open("stdout.bin", ios_base::out | ios_base::binary) : fout.open("stdout.hex");
+
 	fin.open(arquivo);
+	int runTimeLine = 0;
  	while(!fin.eof()){
 		string aux;
-		int value = 0;
-		fin >> aux;
-		
 		regex regInst("[0-9$:]");
+		fin >> aux;
 
 		if (!regex_search(aux, regInst))
-			if (isR(aux))
-				cout << valueInstructionR(fin, aux) << endl;
-			else if (isI(aux))
-				cout << valueInstructionI(fin, aux) << endl;
-			else if(isJ(aux))
-				for(int i = 0; i < 2; i++){
-					if(aux == j[i].type){
-						value = j[i].op;
-						value = value << 26;
-
-						fin >> aux;
-						// value = value | std::stoi(aux);
-					}
-				}
+			if (isR(aux)) {
+				unsigned instruction = valueInstructionR(fin, aux, runTimeLine);
+				res == '1' ? fout.write((char *) &instruction, sizeof(unsigned)) : fout << hex << instruction << endl;
+			}
+			else if (isI(aux)) {
+				unsigned instruction = valueInstructionI(fin, aux, l, runTimeLine);
+				res == '1' ? fout.write((char *) &instruction, sizeof(unsigned)) : fout << hex << instruction << endl;
+			}
+			else if (isJ(aux)) {
+				unsigned instruction = valueInstructionJ(fin, aux, l, runTimeLine);
+				res == '1' ? fout.write((char *) &instruction, sizeof(unsigned)) : fout << hex << instruction << endl;
+			}
 			else 
 				exit(EXIT_FAILURE);
-
-		cout << value;
 	}
+
+	cout << "\nArquivo salvo com o nome 'stdout'";
+
+	fin.close();
+	fout.close();
 }
 
-unsigned valueInstructionR(std::ifstream& fin, string aux) {
+bool isR(string str) {
+	for (int i = 0; i < 16; i++) {
+		if (str == typeR[i].type)
+			return true;
+	}
+	return false;
+}
+
+bool isI(string str) {
+	for (int k = 0; k < 11; k++) {
+		if (str == typeI[k].type)
+			return true;
+	}
+	return false;
+}
+
+bool isJ(string str) {
+	for (int i = 0; i < 2; i++) {
+		if (str == typeJ[i].type)
+			return true;
+	}
+	return false;
+}
+
+bool isLabel(string aux) {
+	regex reg("[:]");
+	if (regex_search(aux, reg))
+		return true;
+	return false;
+}
+
+
+unsigned valueInstructionR(std::ifstream& fin, string aux, int& runTimeLine) {
 	unsigned value = 0;
-	for (int i = 0; i < 16; i++)
-		if (aux == r[i].type && (aux != r[0].type && aux != r[1].type)) {
+	if (aux == typeR[0].type || aux == typeR[1].type) {
+		int regs[3]{};
+
+		aux == typeR[0].type ? value = value | typeR[0].op : value = value | typeR[1].op;
+		value = value << 6;
+
+		for (int j = 0; j < 2; j++) {
+			fin >> aux; // $t0,							
+			aux.pop_back(); // $t0
+			int regis;
+			for (regis = 0; regis < 32; regis++)
+				if (aux == registradores[regis])
+					break;
+			regs[j] = regis; // Salva o valor dos registradores
+		}
+
+		fin >> aux;
+		regs[2] = stoi(aux);
+		value = value << 5; // rs
+
+		value = value | regs[1]; // rt
+		value = value << 5;
+
+		value = value | regs[0]; // rd
+		value = value << 5;
+
+		value = value | regs[2]; // shamt
+
+		value = value << 6;
+		aux == typeR[0].type ? value = value | typeR[0].funct : value = value | typeR[1].funct;
+
+		fin.get() == '\n' ? runTimeLine++ : runTimeLine = runTimeLine; // Incremento de linha em tempo de execu��o
+
+		return value;
+	}
+
+	for (int i = 2; i < 16; i++) {
+		if (aux == typeR[i].type) {
 			int regs[3];
 
-			value = value | r[i].op;
+			value = value | typeR[i].op;
 			value = value << 6;
 
 			for (int j = 0; j < 3; j++) {
@@ -139,52 +223,56 @@ unsigned valueInstructionR(std::ifstream& fin, string aux) {
 			value = value << 5;
 
 			value = value << 6;
-			value = value | r[i].funct;
+			value = value | typeR[i].funct;
+
+			fin.get() == '\n' ? runTimeLine++ : runTimeLine = runTimeLine; // Incremento de linha em tempo de execu��o
+
+			break;
 		}
-		else if (aux == r[0].type || aux == r[1].type) {
-			int regs[3];
-			int func;
-
-			aux == r[0].type ? func = 0 : func = 2;
-
-			value = value | r[i].op;
-			value = value << 6;
-
-			for (int j = 0; j < 2; j++) {
-				fin >> aux; // $19,							
-				aux.erase(3, 3);
-				int regis;
-				for (regis = 0; regis < 32; regis++)
-					if (aux == registradores[regis])
-						break;
-				regs[j] = regis;
-			}
-
-			fin >> aux;
-			regs[2] = stoi(aux);
-			value = value << 5; // rs
-
-			value = value | regs[1]; // rt
-			value = value << 5;
-
-			value = value | regs[0]; // rd
-			value = value << 5;
-
-			value = value | regs[2]; // shamt
-			
-			value = value << 6;
-			value = value | func;
-		}
+	}
 	return value;
 }
 
-unsigned valueInstructionI(ifstream& fin, string aux) {
-	unsigned regs[2];
+unsigned valueInstructionI(ifstream& fin, string aux, vector<Label> l, int& runTimeLine) {
+	unsigned regs[2]{};
 	int value = 0;
-	for (int j = 0; j < 11; j++) {
-		if (aux == i[j].type) {
-			value = value | i[j].op;
-			value = value << 6;
+
+	if (aux == typeI[0].type || aux == typeI[1].type) {
+		aux == typeI[0].type ? value = value | typeI[0].op : value = value | typeI[1].op;
+		value = value << 5;
+		// Pegar a linha atual da execu��o
+		for (int j = 0; j < 2; j++) {
+			fin >> aux; // $19,							
+			aux.pop_back();
+			int regis;
+			for (regis = 0; regis < 32; regis++)
+				if (aux == registradores[regis])
+					break;
+			regs[j] = regis;
+		}
+		value = value | regs[0];
+		value = value << 5;
+
+		value = value | regs[1];
+		value = value << 16;
+
+		fin >> aux;
+		for (int j = 0; j < l.size(); j++) {
+			if (aux == l[j].label) {
+				int p1 = initialAddress - (l[j].line + (runTimeLine + 1) * 4);
+				p1 = 65535 + p1 / 4;
+				value = value | p1 + 1; // Pega o numero de instrucoes acima, -12 por exemplo, 3 instrucoes acima e faz um pseudo complemento de 2 para 16 bits
+			}
+		}
+
+		fin.get() == '\n' ? runTimeLine++ : runTimeLine = runTimeLine; // Incremento de linha em tempo de execu��o
+		return value;
+	}
+
+	for (int j = 2; j < 10; j++) {
+		if (aux == typeI[j].type) {
+			value = value | typeI[j].op;
+			value = value << 5;
 
 			for (int k = 0; k < 2; k++) {
 				fin >> aux;
@@ -200,13 +288,32 @@ unsigned valueInstructionI(ifstream& fin, string aux) {
 			value = value << 5;
 
 			value = value | regs[0];
-			value = value << 5;
+			value = value << 16;
 
 			fin >> aux;
 			value = value | stoi(aux);
 
-			cout << value << endl;
+			fin.get() == '\n' ? runTimeLine++ : runTimeLine = runTimeLine; // Incremento de linha em tempo de execu��o
+
+			break;
 		}
-		return value;
 	}
+	return value;
+}
+
+unsigned valueInstructionJ(ifstream& fin, string aux, vector<Label> l, int& runTimeLine) {
+	int value = 0;
+	if (aux == typeJ[0].type) {
+		value = value | typeJ[0].op;
+		value = value << 26;
+
+		fin >> aux;
+		for (int i = 0; i < l.size(); i++) {
+			if (aux == l[i].label)
+				value = value | (l[i].line/4);
+		}
+
+		fin.get() == '\n' ? runTimeLine++ : runTimeLine = runTimeLine; // Incremento de linha em tempo de execu��o
+	}
+	return value;
 }
